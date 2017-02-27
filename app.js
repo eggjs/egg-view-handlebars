be5b11df5d130803a9cab4804cf558ce65ee1fab
huangyanxiong <huangyanxiong2013@gmail.com>
2017-02-27 23:08:20 +0800

    feat: follow #398

--- app.js
+++ app.js
@@ -1,5 +1,33 @@
 'use strict';
 
+const COMPILE = Symbol('compile');
+const fs = require('fs');
+const handlebars = require('handlebars');
+
 module.exports = app => {
-  app.view.use('handlebars', require('./lib/view'));
+  class HandlebarsView {
+    constructor(ctx) {
+      this.app = ctx.app;
+    }
+
+    render(name, context) {
+      return new Promise((resolve, reject) => {
+        fs.readFile(name, 'utf8', (err, tpl) => {
+          if (err) return reject(err);
+          resolve(this[COMPILE](tpl, context));
+        });
+      });
+    }
+
+    renderString(tpl, context) {
+      return new Promise(resolve => {
+        resolve(this[COMPILE](tpl, context));
+      });
+    }
+
+    [COMPILE](tpl, context) {
+      return handlebars.compile(tpl, Object.assign(this.app.config.handlebars, this.app.config.view) || {})(context);
+    }
+  }
+  app.view.use('handlebars', HandlebarsView);
 };
