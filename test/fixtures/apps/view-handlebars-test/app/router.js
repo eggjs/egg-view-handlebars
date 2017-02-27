0a961a4cc1b05e974c788ec582e4eb31aca5ae85
huangyanxiong <huangyanxiong2013@gmail.com>
2017-02-27 15:37:46 +0800

    test:add render,renderString

--- test/fixtures/apps/view-handlebars-test/app/router.js
+++ test/fixtures/apps/view-handlebars-test/app/router.js
@@ -4,4 +4,12 @@ module.exports = app => {
   app.get('/', function* () {
     this.body = 'hi, ' + app.plugins.handlebars.name;
   });
+
+  app.get('/renderString',function* () {
+    this.body = yield this.renderString('{{title}}',{title:'renderString'});
+  });
+
+  app.get('/render',function* () {
+    yield this.render('home',{title:'render'});
+  });
 };
