f6f0955be8dc734e09f9fb8c6189c9871ca22e0a
huangyanxiong <huangyanxiong2013@gmail.com>
2017-03-03 21:55:05 +0800

    core: Merge options and change Promise.resolve in sync mode

--- app.js
+++ app.js
@@ -10,23 +10,21 @@ module.exports = app => {
       this.app = ctx.app;
     }
 
-    render(name, context) {
+    render(name, context, options) {
       return new Promise((resolve, reject) => {
         fs.readFile(name, 'utf8', (err, tpl) => {
           if (err) return reject(err);
-          resolve(this[COMPILE](tpl, context));
+          resolve(this[COMPILE](tpl, context, options));
         });
       });
     }
 
-    renderString(tpl, context) {
-      return new Promise(resolve => {
-        resolve(this[COMPILE](tpl, context));
-      });
+    renderString(tpl, context, options) {
+      return Promise.resolve(this[COMPILE](tpl, context, options));
     }
 
-    [COMPILE](tpl, context) {
-      return handlebars.compile(tpl, Object.assign(this.app.config.handlebars, this.app.config.view) || {})(context);
+    [COMPILE](tpl, context, options) {
+      return handlebars.compile(tpl, Object.assign(this.app.config.handlebars, options) || {})(context);
     }
   }
   app.view.use('handlebars', HandlebarsView);
