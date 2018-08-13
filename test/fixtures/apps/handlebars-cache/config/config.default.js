9732dac804418296f5a4573b476d11fe26f26355
daifee <mr_zhangjiayong@163.com>
2018-08-13 14:34:41 +0800

    feature-cache: add test

---
+++ test/fixtures/apps/handlebars-cache/config/config.default.js
@@ -0,0 +1,29 @@
+'use strict';
+/**
+ * app keys
+ * @type {string}
+ */
+exports.keys = 'e6099001cb0a0ec3eb8b0e1a54b27060';
+/**
+ * view config
+ * @member
+ * @property defaultViewEngine: string setup default view engine
+ * @property defaultExtension: string template file extension
+ * @property mapping: Object {string: string}
+ */
+exports.view = {
+  defaultViewEngine: 'handlebars',
+  defaultExtension: '.hbs',
+  mapping: {
+    '.hbs': 'handlebars',
+  },
+};
+
+/**
+ * handlebars config
+ * @member
+ * @property {boolean} cache cache template by filename
+ */
+exports.handlebars = {
+  cache: true,
+};
---
+++ test/handlebars-cache.test.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const request = require('supertest');
+const mm = require('egg-mock');
+const fs = require('fs');
+const path = require('path');
+
+describe('test/handlebars-cache.test.js', () => {
+  let app;
+
+  function writeHbsFile(app, content) {
+    const tplPath = path.resolve(app.config.view.root[0], './cache.hbs');
+    fs.writeFileSync(tplPath, content, 'utf-8');
+  }
+
+  before(() => {
+    app = mm.app({
+      baseDir: 'apps/handlebars-cache',
+    });
+    return app.ready(() => {
+      writeHbsFile(app, 'version-1');
+    });
+  });
+
+  after(() => {
+    writeHbsFile(app, 'version-1');
+    app.close();
+  });
+  afterEach(mm.restore);
+
+  it.only('cache compiled', () => {
+    return request(app.callback())
+      .get('/cache')
+      .expect(/version-1/)
+      .expect(200)
+      .then(() => {
+        // 修改模板
+        writeHbsFile(app, 'version-2');
+
+        return request(app.callback())
+          .get('/cache')
+          .expect(/version-1/)
+          .expect(200);
+      });
+  });
+});
